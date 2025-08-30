559267c33d9ecf3fe2f04d6295e64479fca82af3
gulcinhebibillayeva <gulcinhebillayeva@gmail.com>
2025-08-31 02:53:55 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ADO_NET_07_Homework;
+using Microsoft.EntityFrameworkCore;
+
+
+using LibraryContext db = new();
+
+
+while (true)
+{
+    Console.WriteLine("Choose");
+
+    Console.WriteLine("1.Books by topic");
+    Console.WriteLine("2.Books by Category");
+    Console.WriteLine("3.Books by Author");
+    if (!int.TryParse(Console.ReadLine(), out int choise))
+    {
+        Console.WriteLine("Please enter number\n");
+        continue;
+    }
+    if (choise == 1)
+    {
+        var topics = db.Themes.Include(a => a.Books).ToList();
+        foreach (var topic in topics)
+        {
+            Console.WriteLine($"******{topic.Id} {topic.Name}");
+            foreach (var book in topic.Books)
+            {
+                Console.WriteLine($"{book.Name}");
+            }
+        }
+    }
+
+
+    else if (choise == 2)
+    {
+        var categories = db.Categories.Include(a => a.Books).ToList();
+        foreach (var category in categories)
+        {
+            Console.WriteLine($"******{category.Id} {category.Name}");
+            foreach (var book in category.Books)
+            {
+                Console.WriteLine($"{book.Name}");
+            }
+        }
+    }
+
+    else if (choise == 3)
+    {
+        var authors = db.Authors.Include(a => a.Books).ToList();
+        foreach (var author in authors)
+        {
+            Console.WriteLine($"******{author.FirstName} {author.LastName}");
+            foreach (var book in author.Books)
+            {
+                Console.WriteLine($"{book.Name}");
+            }
+        }
+    }
+    
+    
+    
+}
